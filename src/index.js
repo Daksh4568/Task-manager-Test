8f5e07339669f8081bb3e458b66e9aeeb109e28f
Arun Luthra <arun@amergingtech.com>
2024-09-05 10:59:02 +0530

    index port

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.use(taskRouter)
 //With middleware: new request -> pass through middleware -> run route handler
 
 
-app.listen(PORT , '0.0.0.0', ()=>{
+app.listen(port , '0.0.0.0', ()=>{
     console.log('Server is running on port ' + port)
 })
 
